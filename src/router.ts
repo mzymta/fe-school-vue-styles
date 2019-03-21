a0a3cdc407293671f6e94a19a5776a886a38df09
mzymta <ekonyakhin@gmail.com>
2019-03-21 13:26:15 +0300

    VS-4 class binding

--- src/router.ts
+++ src/router.ts
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Example1 from './views/Example1.vue';
 import Example2 from './views/Example2.vue';
+import Example3 from './views/Example3.vue';
 
 Vue.use(Router);
 
 export enum RouteNames {
   example1 = 'example1',
-  example2 = 'example2'
+  example2 = 'example2',
+  example3 = 'example3'
 }
 
 export default new Router({
@@ -24,6 +26,11 @@ export default new Router({
       path: '/2',
       name: RouteNames.example2,
       component: Example2
+    },
+    {
+      path: '/3',
+      name: RouteNames.example3,
+      component: Example3
     }
   ]
 });
---
+++ src/views/Example3.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="example-3 section">
+    <h2 class="title is-3">Example 3: class binding</h2>
+    <div class="field">
+      <button
+        :class="{'is-disabled': isDisabled, 'is-primary': isPrimary, 'is-large': isLarge}"
+        class="button">Some text</button>
+    </div>
+    <div class="box">
+      <h3 class="title is-5">Controls</h3>
+      <div class="field">
+        <button
+          @click="isDisabled = !isDisabled"
+          class="button">disabled: {{ isDisabled }}</button>
+      </div>
+      <div class="field">
+        <button
+          @click="isPrimary = !isPrimary"
+          class="button">primary: {{ isPrimary }}</button>
+      </div>
+      <div class="field">
+        <button
+          @click="isLarge = !isLarge"
+          class="button">large: {{ isLarge }}</button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+
+  interface IExample3Data {
+    isDisabled: boolean;
+    isPrimary: boolean;
+    isLarge: boolean;
+  }
+
+  export default Vue.extend({
+    data(): IExample3Data {
+      return {
+        isDisabled: false,
+        isPrimary: false,
+        isLarge: false
+      };
+    },
+    computed: {
+      computedClass(): {[key: string]: boolean} {
+        return {'is-disabled': this.isDisabled, 'is-primary': this.isPrimary, 'is-large': this.isLarge};
+      }
+    }
+  });
+</script>
